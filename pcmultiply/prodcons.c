627206c22bc6dc3234410061e40b127012d343f9
Nathan Hinthorne <nathanrh856@gmail.com>
2024-11-13 15:20:11 -0800

    add comments

--- pcmultiply/prodcons.c
+++ pcmultiply/prodcons.c
@@ -9,7 +9,7 @@
  * TCSS 422 - Operating Systems
  */
 
- // Include only libraries for this module
+// Include only libraries for this module
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -19,20 +19,20 @@
 #include "pcmatrix.h"
 #include "prodcons.h"
 
-Matrix** bigMatrix;
+Matrix **bigMatrix;
 int headIndex = 0;
 int tailIndex = 0;
 int bufferSize = 0;
 
-counters_t* counters;
+counters_t *counters;
 
 // Probably init in main.
-Matrix** initBoundedBuffer()
+Matrix **initBoundedBuffer()
 {
-  bigMatrix = (Matrix**)malloc(sizeof(Matrix*) * BOUNDED_BUFFER_SIZE);
+  bigMatrix = (Matrix **)malloc(sizeof(Matrix *) * BOUNDED_BUFFER_SIZE);
   for (int n = 0; n < BOUNDED_BUFFER_SIZE; n++)
   {
-    bigMatrix[n] = (Matrix*)malloc(sizeof(Matrix));
+    bigMatrix[n] = (Matrix *)malloc(sizeof(Matrix));
   }
 
   // TODO fix segmentation fault.
@@ -42,10 +42,16 @@ Matrix** initBoundedBuffer()
   return bigMatrix;
 }
 
+counters_t *initCounters()
+{
+  // TODO set counters to some initial value (using malloc)
+  // TODO call init_cnt() for each counter
+}
+
 // Define Locks, Condition variables, and so on here
 
 // Bounded buffer put() get()
-int put(Matrix* value)
+int put(Matrix *value)
 {
   bigMatrix[headIndex] = value;
   printf("PUT Matrix:");
@@ -61,11 +67,11 @@ int put(Matrix* value)
   bufferSize++;
 }
 
-Matrix* get()
+Matrix *get()
 {
   assert(bufferSize > 0); // there must be at least 1 matrix to retrieve
 
-  Matrix* value = bigMatrix[tailIndex];
+  Matrix *value = bigMatrix[tailIndex];
 
   printf("GET Matrix:");
   DisplayMatrix(value, stdout);
@@ -82,16 +88,16 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t not_full = PTHREAD_COND_INITIALIZER;
 pthread_cond_t not_empty = PTHREAD_COND_INITIALIZER;
 
-
 // Matrix PRODUCER worker thread
-void* prod_worker(void* arg)
+void *prod_worker(void *arg)
 {
-  Matrix* randomMatrix = GenMatrixRandom();
+  // TODO loop this WHOLE section of code using a for loop. repeat until the given global constant for num of matrices has been reached
+  Matrix *randomMatrix = GenMatrixRandom();
 
   // critical section
   pthread_mutex_lock(&mutex);
 
-  //TODO keep waiting when buffer is full
+  // keep waiting when buffer is full
   while (bufferSize >= BOUNDED_BUFFER_SIZE)
   {
     pthread_cond_wait(&not_full, &mutex);
@@ -99,10 +105,10 @@ void* prod_worker(void* arg)
 
   put(randomMatrix);
 
-  //TODO increment counter (indicating a new matrix was added)
+  // increment counter (indicating a new matrix was added)
   increment_cnt(&counters->cons);
 
-  //TODO signal consumers
+  // signal consumers
   pthread_cond_signal(&not_empty);
 
   pthread_mutex_unlock(&mutex);
@@ -111,9 +117,8 @@ void* prod_worker(void* arg)
 }
 
 // Matrix CONSUMER worker thread
-void* cons_worker(void* arg)
+void *cons_worker(void *arg)
 {
-
-  // get();
+  // TODO remember to call FreeMatrix() after you're done using it
   return NULL;
 }
